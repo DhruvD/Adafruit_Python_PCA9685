599a242db1bc894e97e68f9540642210da627a74
Dhruv Diddi <DhruvD@users.noreply.github.com>
2017-04-27 06:28:31 -0500

    Update simpletest.py

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -1,7 +1,3 @@
-# Simple demo of of the PCA9685 PWM servo/LED controller library.
-# This will move channel 0 from min to max position repeatedly.
-# Author: Tony DiCola
-# License: Public Domain
 from __future__ import division
 import time
 
@@ -9,57 +5,179 @@ import time
 import Adafruit_PCA9685
 
 
-# Uncomment to enable debug output.
-#import logging
-#logging.basicConfig(level=logging.DEBUG)
-
-# Initialise the PCA9685 using the default address (0x40).
-#pwm = Adafruit_PCA9685.PCA9685()
-pwm = Adafruit_PCA9685.PCA9685(address=0x70
-
-# Alternatively specify a different address and/or bus:
-#pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
-
-# Configure min and max servo pulse lengths
-servo_min = 150  # Min pulse length out of 4096
-servo_max = 600  # Max pulse length out of 4096
-                               
 def angle_to_pulse(angle):
-    return (2.5*angle)+150
+  angle = angle % 180
+  return (2.5*angle)+150
 
-# Helper function to make setting a servo pulse width simpler.
-def set_servo_pulse(channel, pulse):
-    pulse_length = 1000000    # 1,000,000 us per second
-    pulse_length //= 60       # 60 Hz
-    print('{0}us per period'.format(pulse_length))
-    pulse_length //= 4096     # 12 bits of resolution
-    print('{0}us per bit'.format(pulse_length))
-    pulse *= 1000
-    pulse //= pulse_length
-    pwm.set_pwm(channel, 0, pulse)
+def move_motor(motorN, inputAngle):
+  motorN = (motorN-1)*4
+  
+  pwm = Adafruit_PCA9685.PCA9685(address=0x40)
 
-# Set frequency to 60hz, good for servos.
-pwm.set_pwm_freq(60)
+  servo_min = 150  # Min pulse length out of 4096
+  servo_max = 600  # Max pulse length out of 4096
+                               
+  pwm.set_pwm_freq(60)
+  
+  pwm.set_pwm(motorN, 0, angle_to_pulse(inputAngle))
+  time.sleep(2)
+  pwm.set_pwm(0, 0, angle_to_pulse(90))
+  pwm.set_pwm(4, 0, angle_to_pulse(90))
+  pwm.set_pwm(8, 0, angle_to_pulse(90))
+  
+def move_lamp_no():
+  
+  pwm = Adafruit_PCA9685.PCA9685(address=0x40)
 
-print('Moving servo on channel 0, press Ctrl-C to quit...')
-while True:
-    # Move servo on channel O between extremes.
-    pwm.set_pwm(0, 0, angle_to_pulse(30))
-    time.sleep(1)
-    pwm.set_pwm(0, 0, angle_to_pulse(60))
-    time.sleep(1)
-    pwm.set_pwm(4, 0, angle_to_pulse(90))
-    time.sleep(1)
-    pwm.set_pwm(4, 0, angle_to_pulse(120))
-    time.sleep(1)
-    pwm.set_pwm(8, 0, angle_to_pulse(30))
-    time.sleep(1)
-    pwm.set_pwm(8, 0, angle_to_pulse(180))
-    time.sleep(1)
-                               
-                               
-                               
-                               
-                               
-              
-                            
+  servo_min = 150  # Min pulse length out of 4096
+  servo_max = 600  # Max pulse length out of 4096         
+  pwm.set_pwm_freq(60)
+  
+  pwm.set_pwm(0, 0, angle_to_pulse(30))
+  
+  for i in range(0,30):
+    pwm.set_pwm(0, 0, angle_to_pulse(30+i))
+    time.sleep(0.05)
+      
+  pwm.set_pwm(0, 0, angle_to_pulse(60))
+  
+  for i in range(0,30):
+    pwm.set_pwm(0, 0, angle_to_pulse(60-i))
+    time.sleep(0.05)
+    
+    
+  pwm.set_pwm(0, 0, angle_to_pulse(30))
+  
+  for i in range(0,30):
+    pwm.set_pwm(0, 0, angle_to_pulse(30+i))
+    time.sleep(0.05)
+      
+  pwm.set_pwm(0, 0, angle_to_pulse(60))
+  
+  for i in range(0,30):
+    pwm.set_pwm(0, 0, angle_to_pulse(60-i))
+    time.sleep(0.05)
+  
+  pwm.set_pwm(4, 0, angle_to_pulse(90))
+      
+      
+def move_lamp_yes():
+  pwm = Adafruit_PCA9685.PCA9685(address=0x40)
+
+  servo_min = 150  # Min pulse length out of 4096
+  servo_max = 600  # Max pulse length out of 4096         
+  pwm.set_pwm_freq(60)
+  
+  pwm.set_pwm(4, 0, angle_to_pulse(30))
+  
+  for i in range(0,30):
+    pwm.set_pwm(4, 0, angle_to_pulse(30+i))
+    time.sleep(0.05)
+      
+  pwm.set_pwm(4, 0, angle_to_pulse(60))
+  
+  for i in range(0,30):
+    pwm.set_pwm(4, 0, angle_to_pulse(60-i))
+    time.sleep(0.05)
+    
+    
+  pwm.set_pwm(4, 0, angle_to_pulse(30))
+  
+  for i in range(0,30):
+    pwm.set_pwm(4, 0, angle_to_pulse(30+i))
+    time.sleep(0.05)
+      
+  pwm.set_pwm(4, 0, angle_to_pulse(60))
+  
+  for i in range(0,30):
+    pwm.set_pwm(4, 0, angle_to_pulse(60-i))
+    time.sleep(0.05)
+    
+  pwm.set_pwm(4, 0, angle_to_pulse(90))
+                
+def move_lamp_yes_1():
+  pwm = Adafruit_PCA9685.PCA9685(address=0x40)
+
+  servo_min = 150  # Min pulse length out of 4096
+  servo_max = 600  # Max pulse length out of 4096         
+  pwm.set_pwm_freq(60)
+  
+  pwm.set_pwm(8, 0, angle_to_pulse(30))
+  
+  for i in range(0,30):
+    pwm.set_pwm(8, 0, angle_to_pulse(30+i))
+    time.sleep(0.05)
+      
+  pwm.set_pwm(8, 0, angle_to_pulse(60))
+  
+  for i in range(0,30):
+    pwm.set_pwm(8, 0, angle_to_pulse(60-i))
+    time.sleep(0.05)
+    
+    
+  pwm.set_pwm(8, 0, angle_to_pulse(30))
+  
+  for i in range(0,30):
+    pwm.set_pwm(8, 0, angle_to_pulse(30+i))
+    time.sleep(0.05)
+      
+  pwm.set_pwm(8, 0, angle_to_pulse(60))
+  
+  for i in range(0,30):
+    pwm.set_pwm(8, 0, angle_to_pulse(60-i))
+    time.sleep(0.05)
+    
+  pwm.set_pwm(8, 0, angle_to_pulse(90))
+  
+def motor_test():
+  pwm = Adafruit_PCA9685.PCA9685(address=0x40)
+
+  servo_min = 150  # Min pulse length out of 4096
+  servo_max = 600  # Max pulse length out of 4096         
+  pwm.set_pwm_freq(60)
+  
+  pwm.set_pwm(0, 0, angle_to_pulse(30))
+  
+  for i in range(0,30):
+    pwm.set_pwm(0, 0, angle_to_pulse(30+i))
+    time.sleep(0.05)
+      
+  pwm.set_pwm(0, 0, angle_to_pulse(60))
+  
+  for i in range(0,30):
+    pwm.set_pwm(0, 0, angle_to_pulse(60-i))
+    time.sleep(0.05)
+  
+  pwm.set_pwm(4, 0, angle_to_pulse(30))
+  
+  for i in range(0,30):
+    pwm.set_pwm(4, 0, angle_to_pulse(30+i))
+    time.sleep(0.05)
+      
+  pwm.set_pwm(4, 0, angle_to_pulse(60))
+  
+  for i in range(0,30):
+    pwm.set_pwm(4, 0, angle_to_pulse(60-i))
+    time.sleep(0.05)
+    
+  pwm.set_pwm(8, 0, angle_to_pulse(30))
+  
+  for i in range(0,30):
+    pwm.set_pwm(8, 0, angle_to_pulse(30+i))
+    time.sleep(0.05)
+      
+  pwm.set_pwm(8, 0, angle_to_pulse(60))
+  
+  for i in range(8,30):
+    pwm.set_pwm(0, 0, angle_to_pulse(60-i))
+    time.sleep(0.05)
+    
+  pwm.set_pwm(0, 0, angle_to_pulse(90))
+  pwm.set_pwm(4, 0, angle_to_pulse(90))
+  pwm.set_pwm(8, 0, angle_to_pulse(90))
+  
+  
+  
+  
+  
+                
